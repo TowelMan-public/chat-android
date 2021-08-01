f13bbef64bde2a2d33ab836610c3b2e87fcf4597
TowelMan-public <OdagiriTomoya@outlook.jp>
2021-08-01 21:02:17 +0900

    トークルームのView完成

---
+++ app/src/main/java/towelman/server_on/net/chat_android/TalkRoomView.kt
@@ -0,0 +1,35 @@
+package towelman.server_on.net.chat_android
+
+import android.content.Context
+import android.os.Parcelable
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+
+/**
+ * トークルームのView
+ */
+class TalkRoomView(context : Context, attributeSet: AttributeSet) : LinearLayout(context, attributeSet) {
+    init {
+        View.inflate(context, R.layout.talk_room_view, this)
+    }
+
+    /**
+     * トークルーム名をセットする
+     *
+     * @param talkRoomName トークルーム名
+     */
+    fun setTalkRoomName(talkRoomName: String){
+        findViewById<TextView>(R.id.talkRoomNameTextView).setText(talkRoomName, TextView.BufferType.NORMAL)
+    }
+
+    /**
+     * トークルームの通知数をセットする
+     *
+     * @param talkRoomNoticeCount トークルームの通知数
+     */
+    fun setTalkRoomNoticeCount(talkRoomNoticeCount: Int){
+        findViewById<TextView>(R.id.talkRoomNoticeTextView).setText(talkRoomNoticeCount.toString(), TextView.BufferType.NORMAL)
+    }
+}
